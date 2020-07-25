f67e36328e5d1bd82bf92587a0d1a32dcc6710ab
Chris <chris.hou.88@gmail.com>
2020-07-25 11:11:38 +0900

    7-23

--- ninja-smoothies/src/App.vue
+++ ninja-smoothies/src/App.vue
@@ -7,11 +7,13 @@
 
 <script>
 import Navbar from '@/components/Navbar'
+import Index from '@/components/Index'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Index
   }
 }
 </script>
