6c51fe5b4402dc2204f674ade5b75feb94f1414d
chris <abc@abcbook.kr>
2020-07-26 07:46:28 +0900

    Index

---
+++ ninja2/src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Index">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Ninja Smoothie'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
