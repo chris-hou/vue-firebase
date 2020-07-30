05382682aac377b27fd4a9f0542377480ac81cc2
mura nono <muranono@muraui-MacBookPro.local>
2020-07-30 17:47:19 +0900

    chat

--- chat/src/components/NewMessage.vue
+++ chat/src/components/NewMessage.vue
@@ -3,22 +3,34 @@
         <form @submit.prevent="addMessage">
             <label form="new-message">New Message (enter to add):</label>
             <input type="text" name="new-message" v-model="LNewMessage">
-
+            <p class="red-text" v-if="feedback">{{feedback}}</p>
         </form>
     </div>
 </template>
 <script>
+import db from '@/firebase/init.js'
+
 export default {
     name: 'NewMessage',
     props: ['name'],
     data() {
         return {
-            LNewMessage:null
+            LNewMessage:null,
+            feedback:null
         }
     },
     methods: {
         addMessage() {
-            console.log(this.LNewMessage, this.name, Date.now())
+            if(this.LNewMessage){
+                db.collection('message').add ({
+                    content: this.LNewMessage,
+                    name: this.name,
+                    timestamp: Date.now()
+                })
+            } else {
+                this.feedback = "Enter message to send"
+            }
+            // console.log(this.LNewMessage, this.name, Date.now())
         }
     }
 }
