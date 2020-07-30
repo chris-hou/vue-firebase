ca10e447f8ad6f7b26b4ebd508d2f1bb9deede6e
mura nono <muranono@muraui-MacBookPro.local>
2020-07-30 17:36:28 +0900

    New Message

---
+++ chat/src/components/NewMessage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="new-message">
+        <form @submit.prevent="addMessage">
+            <label form="new-message">New Message (enter to add):</label>
+            <input type="text" name="new-message" v-model="LNewMessage">
+
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NewMessage',
+    props: ['name'],
+    data() {
+        return {
+            LNewMessage:null
+        }
+    },
+    methods: {
+        addMessage() {
+            console.log(this.LNewMessage, this.name, Date.now())
+        }
+    }
+}
+</script>
